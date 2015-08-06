e6815555297b55944a709de87f3ec0aa30bcb608
Teddy Sterne <sternetj@rose-hulman.edu>
2015-08-06 16:08:21 -0400

    Made better notification support when song changes
    You can also now pause the song by clicking on the notification

--- background.js
+++ background.js
@@ -1,98 +1,56 @@
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/*
-  Displays a notification with the current time. Requires "notifications"
-  permission in the manifest file (or calling
-  "Notification.requestPermission" beforehand).
-*/
-// function show() {
-//   var time = /(..)(:..)/.exec(new Date());     // The prettyprinted time.
-//   var hour = time[1] % 12 || 12;               // The prettyprinted hour.
-//   var period = time[1] < 12 ? 'a.m.' : 'p.m.'; // The period of the day.
-//   new Notification(hour + time[2] + ' ' + period, {
-//     icon: '48.png',
-//     body: 'Time to make the toast.'
-//   });
-// }
-
-// // Conditionally initialize the options.
-// if (!localStorage.isInitialized) {
-//   localStorage.isActivated = true;   // The display activation.
-//   localStorage.frequency = 1;        // The display frequency, in minutes.
-//   localStorage.isInitialized = true; // The option initialization.
-// }
-
-// // Test for notification support.
-// if (window.Notification) {
-//   // While activated, show notifications at the display frequency.
-//   if (JSON.parse(localStorage.isActivated)) { show(); }
-
-//   var interval = 0; // The display interval, in minutes.
-
-//   setInterval(function() {
-//     interval++;
-
-//     if (
-//       JSON.parse(localStorage.isActivated) &&
-//         localStorage.frequency <= interval
-//     ) {
-//       show();
-//       interval = 0;
-//     }
-//   }, 60000);
-// }
-
 console.log("Welcome to Pandora!");
 
-function ti () {
+function ti() {
   this.name = "";
   this.artist = "";
   this.album = "";
   this.image = "";
 }
 
-function ti (name, artist, album, imgUrl) {
+function ti(name, artist, album, imgUrl) {
   this.name = name;
   this.artist = artist;
   this.album = album;
   this.image = imgUrl;
 }
 
-ti.prototype.equals = function (obj){
-    return this.name == obj.name &&
-           this.artist == obj.artist &&
-           this.album == obj.album;
+ti.prototype.equals = function(obj) {
+  return this.name == obj.name &&
+    this.artist == obj.artist &&
+    this.album == obj.album;
 
-  }
+}
 
-function init(){
+function init() {
   setInterval(checkForSongChange, 750);
 }
 
 var trackInfo = new ti();
+var isPlaying = true;
+var timeout = null;
 
 function checkForSongChange() {
   var newTi = new ti($(".playerBarSong")[0].text,
-                     $(".playerBarArtist")[0].text,
-                     $(".playerBarAlbum")[0].text,
-                     $(".playerBarArt")[0].src);
+    $(".playerBarArtist")[0].text,
+    $(".playerBarAlbum")[0].text,
+    $(".playerBarArt")[0].src);
 
   var time = $(".elapsedTime")[0].textContent.split(":");
-  var elapsed = parseInt(time[0]) + parseInt(time[1]);
-//http://www.pandora.com/img/no_album_art.png
-  if (!trackInfo.equals(newTi) && elapsed > 0){
+  var elapsed = parseInt(time[0]) * 60 + parseInt(time[1]);
+  var time2 = $(".remainingTime")[0].textContent.split(":");
+  var toGo = parseInt(time2[0]) * -60 + parseInt(time2[1]);
+  //http://www.pandora.com/img/no_album_art.png
+  if (!trackInfo.equals(newTi) &&
+    (trackInfo.image != newTi.image ||
+      (trackInfo.image == "http://www.pandora.com/img/no_album_art.png" && elapsed > 3))) {
     trackInfo = newTi;
 
-    var track  = trackInfo.name;
+    var track = trackInfo.name;
     var artist = trackInfo.artist;
-    var album  = trackInfo.album;
-    var image  = trackInfo.image;
-    var nt = new Notification(track, {
-        icon: image,
-        body: 'by ' + artist + ' on ' + album,
-    });
+    var album = trackInfo.album;
+    var image = trackInfo.image;
+
+    window.title = track + " by " + artist;
 
     // var options = {
     //   iconUrl: image,
@@ -110,7 +68,17 @@ function checkForSongChange() {
     //     }
     //   ],
     //   isClickable: true
-    // }
+    // };
+
+    var options = {
+      icon: image,
+      body: 'by ' + artist + '\r\non ' + album,
+      sticky: true
+    };
+
+    var nt = new Notification(track, options);
+
+
 
     //var nt2 = chrome.notifications.create("PandoraApp", options, function () {});
 
@@ -119,10 +87,28 @@ function checkForSongChange() {
     //     nt2.close();
     // },120000);
 
-    nt.onclick = function () { nt.close();};
-    setTimeout(function(){
-        nt.close();
-    },120000);
+    var noteClicked = function() {
+      if ($($(".pauseButton")[0]).is(":visible")) {
+        $(".pauseButton").click();
+        nt = new Notification("Paused - " + track, options);
+        nt.onclick = noteClicked;
+      } else {
+        this.title = track;
+        $(".playButton").click();
+        nt = new Notification(track, options);
+        nt.onclick = noteClicked;
+        timeout = setTimeout(function() {
+          nt.close();
+        }, (toGo) * 1000);
+      }
+      return false;
+    }
+
+    nt.onclick = noteClicked;
+
+    setTimeout(function() {
+      nt.close();
+    }, (toGo) * 1000);
 
     console.log("Next Song is: " + trackInfo.name);
   }
